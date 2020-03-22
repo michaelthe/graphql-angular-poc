77e44b0601d8f5703ab5acc88b9655036793dd5d
michael theodorides <mc.theodorides@gmail.com>
2020-03-22 14:43:21 +0200

    textinput

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+
 import {Apollo} from "apollo-angular";
 import gql from 'graphql-tag';
 import {map, tap} from "rxjs/operators";
@@ -12,55 +13,68 @@ import {Observable} from "rxjs";
 })
 export class AppComponent {
 
+
   courses: Observable<any>;
 
   constructor(private apollo: Apollo) {
 
-    this.courses = this.apollo.watchQuery<any>({
-      query: gql`
-        query {
-          customers (limit: 1, offset:0) {
-            customerNumber
-            customerName
-            postalCode
-          }
+    // this.courses = this.apollo.watchQuery({
+    //   query: gql`
+    //     query {
+    //       customers (limit: 1, offset:0) {
+    //         customerNumber
+    //         customerName
+    //         postalCode
+    //       }
+    //
+    //       employees (limit: 1) {
+    //         employeeNumber
+    //         lastName
+    //         firstName
+    //         jobTitle
+    //         customers (limit: 2) {
+    //           customerName
+    //         }
+    //       }
+    //
+    //       customer (id:103) {
+    //         customerNumber
+    //         customerName
+    //         contactLastName
+    //         # contactFirstName
+    //         # salesRepEmployeeNumber
+    //         creditLimit
+    //         salesRep {
+    //           employeeNumber
+    //           lastName
+    //           firstName
+    //         }
+    //       }
+    //
+    //       employee (id: 1002) {
+    //         lastName
+    //         firstName
+    //         jobTitle
+    //       }
+    //     }
+    //   `
+    // })
+    // .valueChanges
+    // .pipe(
+    //   tap(result => {
+    //     console.log(result)
+    //   }),
+    //   map(result => result.data),
+    // );
+  }
 
-          employees (limit: 1) {
-            employeeNumber
-            lastName
-            firstName
-            jobTitle
-            customers (limit: 2) {
-              customerName
-            }
-          }
-
-          customer (id:103) {
-            customerNumber
-            customerName
-            contactLastName
-            # contactFirstName
-            # salesRepEmployeeNumber
-            creditLimit
-            salesRep {
-              employeeNumber
-              lastName
-              firstName
-            }
-          }
-
-          employee (id: 1002) {
-            lastName
-            firstName
-            jobTitle
-          }
-        }
-      `
+  execute(query){
+    this.courses = this.apollo.watchQuery({
+      query: gql`query ${query}`
     })
     .valueChanges
     .pipe(
       tap(result => {
-        debugger
         console.log(result)
       }),
       map(result => result.data),
